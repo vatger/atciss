e47063a5bc5885c8f3e798271df3f63b16ad0405
Robin Gloster <mail@glob.in>
2023-08-20 22:10:13 +0200

    atis-afw: fix OBS display

--- atciss-frontend/src/components/Atis.tsx
+++ atciss-frontend/src/components/Atis.tsx
@@ -67,9 +67,9 @@ const Atis = ({ sx }: { sx?: ThemeUIStyleObject }) => {
           </Text>
           <Text variant="atisXL">X</Text>
           <Text>
-            OBS: {z2(obs.getDate())}
-            {z2(obs.getHours())}
-            {z2(obs.getMinutes())}
+            OBS: {z2(obs.getUTCDate())}
+            {z2(obs.getUTCHours())}
+            {z2(obs.getUTCMinutes())}
           </Text>
           <Text variant="primary">{xmc(metar)}</Text>
           <Text>SR: 0000</Text>
