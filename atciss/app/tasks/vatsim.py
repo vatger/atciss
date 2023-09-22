f2d95966249b96c327a59c9c8dd7b1fd9e00ac6a
Robin Gloster <mail@glob.in>
2023-09-22 19:19:13 +0200

    vatsim: refactor

--- atciss/app/tasks/vatsim.py
+++ atciss/app/tasks/vatsim.py
@@ -1,127 +1,13 @@
 import logging
-from dataclasses import dataclass
-from datetime import datetime
-from typing import List, Optional
 from aiohttp import ClientConnectorError
 
 from pydantic import TypeAdapter
 
 from ..utils import AiohttpClient, RedisClient
 from ..views.atis import Atis
-from ..views.vatsim import Controller
+from ..views.vatsim import Controller, VatsimData
 
 log = logging.getLogger(__name__)
-
-
-@dataclass
-class Facility:
-    id: int
-    short: str
-    long: str
-
-
-@dataclass
-class Rating:
-    id: int
-    short: str
-    long: str
-
-
-@dataclass
-class PilotRating:
-    id: int
-    short_name: str
-    long_name: str
-
-
-@dataclass
-class MilitaryRating:
-    id: int
-    short_name: str
-    long_name: str
-
-
-@dataclass
-class FlightPlan:
-    flight_rules: str
-    aircraft: str
-    aircraft_faa: str
-    aircraft_short: str
-    departure: str
-    arrival: str
-    alternate: str
-    deptime: str
-    enroute_time: str
-    fuel_time: str
-    remarks: str
-    route: str
-    revision_id: int
-    assigned_transponder: str
-
-
-@dataclass
-class Pilot:
-    cid: int
-    name: str
-    callsign: str
-    server: str
-    pilot_rating: int
-    military_rating: int
-    latitude: float
-    longitude: float
-    altitude: int
-    groundspeed: int
-    transponder: str
-    heading: int
-    qnh_i_hg: float
-    qnh_mb: int
-    flight_plan: Optional[FlightPlan]
-    logon_time: datetime
-    last_updated: datetime
-
-
-@dataclass
-class Server:
-    ident: str
-    hostname_or_ip: str
-    location: str
-    name: str
-    clients_connection_allowed: int
-    client_connections_allowed: bool
-    is_sweatbox: bool
-
-
-@dataclass
-class Prefile:
-    cid: int
-    name: str
-    callsign: str
-    flight_plan: FlightPlan
-    last_updated: datetime
-
-
-@dataclass
-class General:
-    version: int
-    reload: int
-    update: str
-    update_timestamp: datetime
-    connected_clients: int
-    unique_users: int
-
-
-@dataclass
-class VatsimData:
-    general: General
-    pilots: List[Pilot]
-    controllers: List[Controller]
-    atis: List[Atis]
-    servers: List[Server]
-    prefiles: List[Prefile]
-    facilities: List[Facility]
-    ratings: List[Rating]
-    pilot_ratings: List[PilotRating]
-    military_ratings: List[MilitaryRating]
 
 
 async def fetch_vatsim_data() -> None:
