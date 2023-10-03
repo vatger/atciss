e661cdc14e4a7ef128b50a14bd0be1c98ea85cca
Robin Gloster <mail@glob.in>
2023-10-03 06:45:17 +0200

    sectorcontrols: don't hide if hidden on map
    this is already implemented on mapcontrols
    
    fixes them being hidden on LOA page if sectors are hidden on map

--- atciss-frontend/src/components/SectorControls.tsx
+++ atciss-frontend/src/components/SectorControls.tsx
@@ -12,7 +12,6 @@ import {
 } from "../services/activePositionSlice"
 import { useAppDispatch, useAppSelector } from "../app/hooks"
 import { usePollControllers } from "../services/controllerApi"
-import { selectSectors } from "../services/mapSlice"
 
 export const SectorControls = () => {
   const dispatch = useAppDispatch()
@@ -20,100 +19,91 @@ export const SectorControls = () => {
   const { data } = sectorApi.useGetByRegionQuery()
   usePollControllers()
 
-  const sectors = useAppSelector(selectSectors)
-
   const activePositions = useAppSelector(selectActivePositions)
   const syncedToOnline = useAppSelector(selectSyncedToOnline)
   const selectedPosition = useAppSelector(selectSelectedPosition)
 
   return (
-    sectors && (
-      <>
-        <Box>
-          <label>
-            <input
-              type="checkbox"
-              checked={syncedToOnline}
-              onChange={(e) => dispatch(setSyncedToOnline(e.target.checked))}
-            />
-            Sync to Online Controllers
-          </label>
-        </Box>
-        <Box>
-          <label>
-            <Text variant="label">Selected Position </Text>
-            <select
-              value={selectedPosition ?? undefined}
-              onChange={(e) => dispatch(setSelectedPosition(e.target.value))}
-            >
-              {Object.entries(activePositions)
-                .filter(([_, p]) => p[syncedToOnline ? "online" : "manual"])
-                .map(([id, p]) => (
-                  <option key={id} value={id}>
-                    {id} ({p.position.frequency})
-                  </option>
-                ))}
-            </select>
-          </label>
-        </Box>
-        <Flex sx={{ flex: "none", justifyContent: "space-between" }}>
-          <Button
-            onClick={() => dispatch(enableAllPositions())}
-            disabled={syncedToOnline}
+    <>
+      <Box>
+        <label>
+          <input
+            type="checkbox"
+            checked={syncedToOnline}
+            onChange={(e) => dispatch(setSyncedToOnline(e.target.checked))}
+          />
+          Sync to Online Controllers
+        </label>
+      </Box>
+      <Box>
+        <label>
+          <Text variant="label">Selected Position </Text>
+          <select
+            value={selectedPosition ?? undefined}
+            onChange={(e) => dispatch(setSelectedPosition(e.target.value))}
           >
-            Select All
-          </Button>
-          <Button
-            onClick={() => dispatch(disableAllPositions())}
-            disabled={syncedToOnline}
-          >
-            Deselect All
-          </Button>
-        </Flex>
-        <Box sx={{ overflow: "auto", flex: "auto" }}>
-          {data &&
-            Object.entries(
-              Object.entries(data.positions)
-                .filter(([id]) => !["MMC", "WWC", "GGC"].includes(id))
-                .reduce((acc, [id, p]) => {
-                  return {
-                    ...acc,
-                    [p.pre[0]]: [...(acc[p.pre[0]] ?? []), id],
-                  }
-                }, {} as { [index: string]: string[] }),
-            ).map(([group, positions]) => (
-              <Flex sx={{ flexWrap: "wrap", marginY: 2 }} key={group}>
-                <Text
-                  sx={{ flexBasis: "100%", marginBottom: 1 }}
-                  variant="label"
-                >
-                  {group}
-                </Text>
-                {positions.map((id) => (
-                  <Flex key={id} sx={{ flexBasis: "33%" }}>
-                    <label>
-                      <input
-                        type="checkbox"
-                        checked={
-                          (syncedToOnline
-                            ? activePositions[id]?.online
-                            : activePositions[id]?.manual) ?? false
-                        }
-                        disabled={syncedToOnline}
-                        onChange={(e) =>
-                          dispatch(
-                            setPosition({ id, active: e.target.checked }),
-                          )
-                        }
-                      />
-                      {id}
-                    </label>
-                  </Flex>
-                ))}
-              </Flex>
-            ))}
-        </Box>
-      </>
-    )
+            {Object.entries(activePositions)
+              .filter(([_, p]) => p[syncedToOnline ? "online" : "manual"])
+              .map(([id, p]) => (
+                <option key={id} value={id}>
+                  {id} ({p.position.frequency})
+                </option>
+              ))}
+          </select>
+        </label>
+      </Box>
+      <Flex sx={{ flex: "none", justifyContent: "space-between" }}>
+        <Button
+          onClick={() => dispatch(enableAllPositions())}
+          disabled={syncedToOnline}
+        >
+          Select All
+        </Button>
+        <Button
+          onClick={() => dispatch(disableAllPositions())}
+          disabled={syncedToOnline}
+        >
+          Deselect All
+        </Button>
+      </Flex>
+      <Box sx={{ overflow: "auto", flex: "auto" }}>
+        {data &&
+          Object.entries(
+            Object.entries(data.positions)
+              .filter(([id]) => !["MMC", "WWC", "GGC"].includes(id))
+              .reduce((acc, [id, p]) => {
+                return {
+                  ...acc,
+                  [p.pre[0]]: [...(acc[p.pre[0]] ?? []), id],
+                }
+              }, {} as { [index: string]: string[] }),
+          ).map(([group, positions]) => (
+            <Flex sx={{ flexWrap: "wrap", marginY: 2 }} key={group}>
+              <Text sx={{ flexBasis: "100%", marginBottom: 1 }} variant="label">
+                {group}
+              </Text>
+              {positions.map((id) => (
+                <Flex key={id} sx={{ flexBasis: "33%" }}>
+                  <label>
+                    <input
+                      type="checkbox"
+                      checked={
+                        (syncedToOnline
+                          ? activePositions[id]?.online
+                          : activePositions[id]?.manual) ?? false
+                      }
+                      disabled={syncedToOnline}
+                      onChange={(e) =>
+                        dispatch(setPosition({ id, active: e.target.checked }))
+                      }
+                    />
+                    {id}
+                  </label>
+                </Flex>
+              ))}
+            </Flex>
+          ))}
+      </Box>
+    </>
   )
 }
