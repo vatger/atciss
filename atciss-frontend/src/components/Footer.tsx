c2440e690a2631e4583295c6c95dd543a27ee2f8
Robin Gloster <mail@glob.in>
2023-10-21 17:26:58 +0200

    footer: add github link

--- atciss-frontend/src/components/Footer.tsx
+++ atciss-frontend/src/components/Footer.tsx
@@ -1,8 +1,22 @@
-import { Flex, Text, ThemeUIStyleObject } from "theme-ui"
+import { Flex, Link, Text, ThemeUIStyleObject } from "theme-ui"
 
 const Footer = ({ sx }: { sx?: ThemeUIStyleObject }) => (
-  <Flex sx={{ ...sx, backgroundColor: "primary", p: ".5em" }}>
-    <Text>ATCISS</Text>
+  <Flex
+    sx={{
+      ...sx,
+      backgroundColor: "primary",
+      p: ".5em",
+      justifyContent: "space-between",
+    }}
+  >
+    <Text sx={{ color: "white" }}>ATCISS</Text>
+    <Link
+      sx={{ color: "white" }}
+      target="_blank"
+      href="https://github.com/a3li/atciss"
+    >
+      GitHub
+    </Link>
   </Flex>
 )
 
