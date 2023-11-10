1b884220111feb897528ee00606d9c686f9daa3a
Robin Gloster <mail@glob.in>
2023-11-10 23:10:27 +0100

    frontend: zero padding, handle negative numbers correctly

--- atciss-frontend/src/app/utils.ts
+++ atciss-frontend/src/app/utils.ts
@@ -1,5 +1,5 @@
-export const zn = (n: number) => (x: number | string) =>
-  String(x).padStart(n, "0")
+export const zn = (n: number) => (x: number) =>
+  (x < 0 ? "-" : "") + String(Math.abs(x)).padStart(n, "0")
 export const z2 = zn(2)
 export const z3 = zn(3)
 export const z4 = zn(4)
