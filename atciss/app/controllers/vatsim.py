98ad28c67ce14ba7f860baec28900e7dd2ea3664
Franz Pletz <fpletz@fnordicwalking.de>
2024-01-04 18:06:43 +0100

    remove vatsim status endpoint, ES uses FSD for metars

--- atciss/app/controllers/vatsim.py
+++ atciss/app/controllers/vatsim.py
@@ -2,12 +2,10 @@
 
 from typing import Annotated, List, Optional, cast
 
-from fastapi.responses import PlainTextResponse
 from fastapi import APIRouter, Depends
 
 from pydantic import TypeAdapter
 
-from ...config.application import settings
 from ..views.vatsim import Controller
 from ..controllers.auth import get_user
 from ..models import User
@@ -16,23 +14,6 @@ from ..utils.redis import RedisClient
 
 
 router = APIRouter()
-
-
-@router.get(
-    "/vatsim/status",
-    response_class=PlainTextResponse,
-)
-async def vatsim_status_get() -> str:
-    data = {
-        "url0": "http://data.vatsim.net/vatsim-data.txt",
-        "json3": "https://data.vatsim.net/v3/vatsim-data.json",
-        "url1": "http://data.vatsim.net/vatsim-servers.txt",
-        "servers.live": "http://data.vatsim.net/vatsim-servers.txt",
-        "metar0": f"{settings.BASE_URL}/api/metar/raw",
-        "user0": "http://stats.vatsim.net/search_id.php",
-        "voice0": "afv",
-    }
-    return "\n".join(f"{key}={value}" for key, value in data.items())
 
 
 @router.get(
