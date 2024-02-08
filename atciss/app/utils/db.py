295bdad6951dd945b404fafedb918d4069dc0c0b
Franz Pletz <fpletz@fnordicwalking.de>
2024-02-08 14:04:13 +0100

    fix utils.db typing

--- atciss/app/utils/db.py
+++ atciss/app/utils/db.py
@@ -1,12 +1,14 @@
+from collections.abc import AsyncIterator
+
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 from ...config import settings
 
 engine = create_async_engine(str(settings.DATABASE_DSN), echo=True)
-async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+async_sessionmaker = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 
-async def get_session() -> AsyncSession:
-    async with async_session() as session:
+async def get_session() -> AsyncIterator[AsyncSession]:
+    async with async_sessionmaker() as session:
         yield session
