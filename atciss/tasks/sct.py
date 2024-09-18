7e8c4cbeb3c8ccf76d8b213bf667ef5f50d84e1d
Franz Pletz <fpletz@fnordicwalking.de>
2024-09-18 20:14:59 +0200

    tasks/sct: also clean up airway segments

--- atciss/tasks/sct.py
+++ atciss/tasks/sct.py
@@ -6,12 +6,13 @@ from aiohttp import ClientSession
 from bs4 import BeautifulSoup
 from loguru import logger
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlmodel import select
+from sqlmodel import or_, select
 from taskiq_dependencies import Depends
 
 from atciss.app.utils import get_aiohttp_client
 from atciss.app.utils.db import get_session
 from atciss.app.views import sct_parser
+from atciss.app.views.airway import AirwaySegment
 from atciss.app.views.dfs_aixm import Aerodrome, Navaid
 from atciss.tkq import broker
 
@@ -50,6 +51,12 @@ async def clear_sct_navdata(
     db_session: Annotated[AsyncSession, Depends(get_session)],
 ) -> None:
     for navaid in await db_session.scalars(select(Navaid).where(Navaid.source == "SCT")):
+        for segment in await db_session.scalars(
+            select(AirwaySegment).where(
+                or_(AirwaySegment.start_id == navaid.id, AirwaySegment.end_id == navaid.id)
+            )
+        ):
+            _ = await db_session.delete(segment)
         _ = await db_session.delete(navaid)
     for ad in await db_session.scalars(select(Aerodrome).where(Aerodrome.source == "SCT")):
         _ = await db_session.delete(ad)
