6bba8e5bba21218580696652450f5b07529e5e99
Robin Gloster <mail@glob.in>
2024-10-21 21:31:36 +0200

    metar: lvp is rvr <= 600 not <

--- atciss-frontend/src/services/metarApi.ts
+++ atciss-frontend/src/services/metarApi.ts
@@ -19,7 +19,7 @@ export const ceiling: (metar: Metar) => number | null = (metar) =>
 
 export const xmc: (metar: Metar) => "VMC" | "IMC" | "LVP" = (metar) => {
   const c = ceiling(metar)
-  return (c && c < 200) || metar.rvr.some((rvr) => rvr.low < 600)
+  return (c && c < 200) || metar.rvr.some((rvr) => rvr.low <= 600)
     ? "LVP"
     : (c && c < 1500) || metar.vis.some((v) => v < 5000)
       ? "IMC"
