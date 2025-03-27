cbcfaba5b1b659698a3755b8aad6ec9215b407d6
Robin Gloster <mail@glob.in>
2025-03-27 20:11:12 +0100

    loa: always flush and commit if successful

--- atciss/tasks/loa.py
+++ atciss/tasks/loa.py
@@ -29,9 +29,12 @@ async def fetch_loas(
 
     prev_loa_items = await db_session.execute(select(LoaItem))
     for (loa_item,) in prev_loa_items.all():
-        _ = await db_session.delete(loa_item)
+        await db_session.delete(loa_item)
 
     for idx, loa_item in enumerate(loas):
         db_session.add(LoaItem.model_validate({"id": idx, **loa_item.model_dump()}))
+
+    await db_session.flush()
+    await db_session.commit()
 
     logger.info(f"LoAs: {len(loas)} received")
