14ab008e862df6b85ebb7622b152a4041afb01b1
Robin Gloster <mail@glob.in>
2026-09-15 18:39:44 +0200

    fix: frontend build

--- flake.nix
+++ flake.nix
@@ -143,7 +143,7 @@
               NODE_ENV = "production";
               inherit nodejs;
               npmCommands = [
-                "npm install --include=dev --nodedir=${nodejs}/include/node --loglevel verbose --ignore-scripts"
+                "npm install --include=dev --legacy-peer-deps --nodedir=${nodejs}/include/node --loglevel verbose --ignore-scripts"
                 "npm run build"
               ];
               installPhase = ''
